be991ad4d8a93065ee709c9ba469fefbc0d82fbe
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-04-20 07:21:05 -0700

    revert caching

--- src/euler_maruyama.jl
+++ src/euler_maruyama.jl
@@ -13,14 +13,12 @@ function DiffEqBase.solve(prob::SDEProblem,alg::SimpleEM,args...;
   n = Int((tspan[2] - tspan[1])/dt) + 1
   u = [u0 for i in 1:n]
   t = [tspan[1] + i*dt for i in 0:n-1]
-  dW = [randn(typeof(u0)) for i in 1:n]
-
   sqdt = sqrt(dt)
 
   for i in 2:n
       uprev = u[i-1]
       tprev = t[i-1]
-      u[i] = f(uprev,p,tprev)*dt + sqdt*g(uprev,p,tprev).*dW[i]
+      u[i] = f(uprev,p,tprev)*dt + sqdt*g(uprev,p,tprev).*randn(typeof(u0))
   end
 
   sol = build_solution(prob,alg,t,u,
