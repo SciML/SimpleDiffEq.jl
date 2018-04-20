ea5e0e9977dd28935e292892a35e0b7d2108a924
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-04-20 07:19:04 -0700

    static caching rng

--- src/euler_maruyama.jl
+++ src/euler_maruyama.jl
@@ -1,7 +1,7 @@
-struct SimpleEM end
+struct SimpleEM <: DEAlgorithm end
 export SimpleEM
 
-function DiffEqBase.solve(prob::SDEProblem,alg::SimpleEM;
+function DiffEqBase.solve(prob::SDEProblem,alg::SimpleEM,args...;
                           dt = error("dt required for SimpleEM"))
 
   f = prob.f
@@ -13,13 +13,14 @@ function DiffEqBase.solve(prob::SDEProblem,alg::SimpleEM;
   n = Int((tspan[2] - tspan[1])/dt) + 1
   u = [u0 for i in 1:n]
   t = [tspan[1] + i*dt for i in 0:n-1]
+  dW = [randn(typeof(u0)) for i in 1:n]
 
   sqdt = sqrt(dt)
 
   for i in 2:n
       uprev = u[i-1]
       tprev = t[i-1]
-      u[i] = f(uprev,p,tprev)*dt + sqdt*g(uprev,p,tprev).*randn(typeof(uprev))
+      u[i] = f(uprev,p,tprev)*dt + sqdt*g(uprev,p,tprev).*dW[i]
   end
 
   sol = build_solution(prob,alg,t,u,
