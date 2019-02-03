6d9e73ff6e2ba229672ece7155b9afd43f5faebc
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2019-02-03 08:30:02 -0500

    allow heterogeneous sizes

--- src/tsit5/atsit5.jl
+++ src/tsit5/atsit5.jl
@@ -101,13 +101,6 @@ end
 
 _initialize_ks(u0::AbstractArray{T}) where {T<:Number} = [zero(u0) for i in 1:7]
 function _initialize_ks(u0::Vector{<:AbstractVector})
-    M = length(u0[1])
-    for v in u0
-        M != length(v) && error(
-        "SimpleATsit5 supports Vector{Vector} stepping only when all inner "*
-        "vectors are the same size."
-        )
-    end
     return [[zero(u0[j]) for j in 1:length(u0)] for i in 1:7]
 end
 
@@ -401,49 +394,49 @@ function DiffEqBase.step!(integ::SAT5I{true, S, T}) where {S<:Vector{<:Array}, T
         dt < 1e-14 && error("dt<dtmin")
 
         for j in 1:M
-            for i in 1:L
+            for i in 1:length(integ.u[j])
                 tmp[j][i] = uprev[j][i]+dt*a21*k1[j][i]
             end
         end
 
         f!(k2, tmp, p, t+c1*dt)
         for j in 1:M
-            for i in 1:L
+            for i in 1:length(integ.u[j])
                 tmp[j][i] = uprev[j][i]+dt*(a31*k1[j][i]+a32*k2[j][i])
             end
         end
 
         f!(k3, tmp, p, t+c2*dt)
         for j in 1:M
-            for i in 1:L
+            for i in 1:length(integ.u[j])
                 tmp[j][i] = uprev[j][i]+dt*(a41*k1[j][i]+a42*k2[j][i]+a43*k3[j][i])
             end
         end
 
         f!(k4, tmp, p, t+c3*dt)
         for j in 1:M
-            for i in 1:L
+            for i in 1:length(integ.u[j])
                 tmp[j][i] = uprev[j][i]+dt*(a51*k1[j][i]+a52*k2[j][i]+a53*k3[j][i]+a54*k4[j][i])
             end
         end
 
         f!(k5, tmp, p, t+c4*dt)
         for j in 1:M
-            for i in 1:L
+            for i in 1:length(integ.u[j])
                 tmp[j][i] = uprev[j][i]+dt*(a61*k1[j][i]+a62*k2[j][i]+a63*k3[j][i]+a64*k4[j][i]+a65*k5[j][i])
             end
         end
 
         f!(k6, tmp, p, t+dt)
         for j in 1:M
-            for i in 1:L
+            for i in 1:length(integ.u[j])
                 u[j][i] = uprev[j][i]+dt*(a71*k1[j][i]+a72*k2[j][i]+a73*k3[j][i]+a74*k4[j][i]+a75*k5[j][i]+a76*k6[j][i])
             end
         end
 
         f!(k7, u, p, t+dt)
         for j in 1:M
-            for i in 1:L
+            for i in 1:length(integ.u[j])
                 tmp[j][i] = dt*(btilde1*k1[j][i]+btilde2*k2[j][i]+btilde3*k3[j][i]+btilde4*k4[j][i]+
                             btilde5*k5[j][i]+btilde6*k6[j][i]+btilde7*k7[j][i])
                 tmp[j][i] = tmp[j][i]/(abstol+max(abs(uprev[j][i]),abs(u[j][i]))*reltol)
