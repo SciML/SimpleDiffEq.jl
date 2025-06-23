84166f2f98afd76f1b1bfccb4f13d741d9f65bb7
Christopher Rackauckas <accounts@chrisrackauckas.com>
2025-06-23 14:30:13 +0000

    Add test

--- test/simpletsit5_tests.jl
+++ test/simpletsit5_tests.jl
@@ -77,6 +77,14 @@ tspan = (0.0, 1.0)
 prob = ODEProblem(f, u0, tspan)
 sol1 = solve(prob, SimpleTsit5(), dt = dt)
 
+function ode(x, p, t)
+  dx = sin(x[1])
+  return([dx])
+end
+prob = ODEProblem(ode, [1.0], (0.0, 0.05), nothing)
+sol = solve(prob, SimpleTsit5(), dt = 0.05/11) # On my PC, the integration ends at 0.04545...
+@test sol.t[end] == 0.05 
+
 #=
 using BenchmarkTools
 
